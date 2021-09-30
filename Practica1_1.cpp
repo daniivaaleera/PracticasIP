47a24864f708779a6044dc2e4b9fe29bd4fc1a21
daniivaaleera <91685421+daniivaaleera@users.noreply.github.com>
2021-09-30 17:54:41 +0200

    Add files via upload

---
+++ Practica1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {	
+	int numero1;
+	cout << "Este es mi segundo programa" << endl;
+	cout << "Introduce un número: ";
+	cin >> numero1;
+	cout << "La variable numero1 vale ahora " << numero1 << endl;
+	return 0;
+}
+
